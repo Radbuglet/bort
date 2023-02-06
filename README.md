dcb28f9e62f669c048219f4a390d795ef624dc38
Radbuglet <10578060+Radbuglet@users.noreply.github.com>
2023-02-05 18:30:50 -0800

    Implement proper `Entity` destruction

---
+++ README.md
@@ -0,0 +1,147 @@
+# Geode
+
+A Simple Object Model for Rust
+
+```rust
+use geode::Entity;
+
+let player = Entity::new()
+	.with(Pos(Vec3::ZERO))
+	.with(Vel(Vec3::ZERO))
+	.with(PlayerState { ... });
+
+let chaser = Entity::new()
+	.with(Pos(Vec3::ZERO))
+	.with(ChaserAi { target: Some(player), home: Vec3::ZERO });
+
+player.get_mut::<PlayerState>().update(player.handle());
+
+let pos = &mut *chaser.get_mut::<Pos>();
+let state = chaser.get::<ChaserState>();
+
+pos.0 = pos.0.lerp(match state.target {
+    Some(target) => target.get::<Pos>().0,
+    None => state.home,
+}, 0.2);
+```
+
+## Getting Started
+
+Applications in Geode are made of `Entity` instances. These are `Copy`able references to logical objects in your application. They can represent anything from a player character in a game to a UI widget.
+
+To create one, just call `Entity::new()`.
+
+```rust
+use geode::Entity;
+
+let player = Entity::new();
+```
+
+From there, you can add components to it using either `insert` or `with`:
+
+```rust
+player.insert(Pos(Vec3::ZERO));
+player.insert(Vel(Vec3::ZERO));
+
+// ...which is equivalent to:
+let player = Entity::new()
+	.with(Pos(Vec3::ZERO))
+	.with(Vel(Vec3::ZERO));
+```
+
+...and access them using `get` or `get_mut`:
+
+```rust
+let mut pos = player.get_mut::<Pos>();  // These are essentially `RefCell`s.
+let vel = player.get::<Vel>();
+
+pos.0 += vel.0;
+```
+
+Note that `Entity::new()` doesn't actually return an `Entity`, rather returning an `OwnedEntity`. These expose the exact same interface as an `Entity` but have an additional `Drop` handler (making them non-`Copy`) that automatically `Entity::destroy`s their managed entity when they leave the scope.
+
+You can extract an `Entity` from them using `OwnedEntity::entity(&self)`.
+
+```rust
+let player2 = player;  // (transfers ownership of `OwnedEntity`)
+// player.insert("hello!");
+// ^ `player` is invalid here; use `player2` instead!
+
+let player_ref = player.entity();  // (produces a copyable `Entity` reference)
+let player_ref_2 = player_ref;  // (copies the value)
+
+assert_eq(player_ref, player_ref_2);
+assert(player_ref.is_alive());
+
+drop(player2);  // (dropped `OwnedEntity`; `player_xx` are all dead now)
+
+assert!(!player_ref.is_alive());
+```
+
+Using these `Entity` handles, you can freely reference you object in multiple places without dealing with cloning smart pointers.
+
+```rust
+use geode::Entity;
+
+struct PlayerId(usize);
+
+struct Name(String);
+
+#[derive(Default)]
+struct PlayerRegistry {
+    all: Vec<Entity>,
+    by_name: HashMap<String, Entity>,
+}
+
+impl PlayerRegistry {
+    pub fn add(&mut self, player: Entity) {
+        player.insert(PlayerId(self.all.len()));
+        self.all.push(player);
+        self.by_name.insert(
+            player.get::<Name>().0.clone(),
+            player,
+        );
+    }
+}
+
+let player = Entity::new()
+	.with(Name("foo".to_string()));
+
+let mut registry = PlayerRegistry::default();
+registry.add(player);
+
+println!("Player is at index {}", player.get::<PlayerId>().0);
+```
+
+See the reference documentation for `Entity` and `OwnedEntity` for a complete list of methods.
+
+### A Note on Borrowing
+
+Geode relies quite heavily on runtime borrowing. Although the type system does not prevent you from mutably borrowing the same component more than once, doing so will cause a panic anyways:
+
+```rust
+use geode::Entity;
+
+let foo = Entity::new()
+	.with(vec![3i32]);
+
+let vec1 = foo.get::<Vec<i32>>();  // Ok
+let a = &vec1[0];
+
+let mut vec2 = foo.get_mut::<Vec<i32>>();  // Panics at runtime!
+vec2.push(4);
+```
+
+Components should strive to only borrow from their logical children. For example, it's somewhat expected for a player to access the components of the items in its inventory but it's somewhat unexpected to have that same player access the world state without that state being explicitly passed to it. Maintaining this informal rule should make borrowing dependencies easier to reason about.
+
+Components should also avoid cross-borrowing other components on themselves, since doing so could force the caller to awkwardly drop and reborrow a component guard. Instead, they should take their component set explicitly like so:
+
+**TODO:** Document `extract`.
+
+## Going Data-Oriented
+
+**TODO**
+
+## Going Multi-Threaded
+
+**TODO**
--- src/entity.rs
+++ src/entity.rs
@@ -1,46 +1,251 @@
-use derive_where::derive_where;
-use fnv::FnvHashMap;
 use std::{
     any::{type_name, Any, TypeId},
     borrow::Borrow,
     cell::{Ref, RefCell, RefMut},
-    collections::hash_map,
-    iter::repeat_with,
+    hash, iter,
     num::NonZeroU64,
     sync::atomic::{AtomicU64, Ordering},
 };
 
-// === Database === //
+// === Helpers === //
+
+type FxHashBuilder = hash::BuildHasherDefault<rustc_hash::FxHasher>;
+type FxHashMap<K, V> = hashbrown::HashMap<K, V, FxHashBuilder>;
+type FxHashSet<T> = hashbrown::HashSet<T, FxHashBuilder>;
+
+fn hash_iter<H, E, I>(state: &mut H, iter: I)
+where
+    H: hash::Hasher,
+    E: hash::Hash,
+    I: IntoIterator<Item = E>,
+{
+    for item in iter {
+        item.hash(state);
+    }
+}
+
+fn merge_iters<I, A, B>(a: A, b: B) -> impl Iterator<Item = I>
+where
+    I: Ord,
+    A: IntoIterator<Item = I>,
+    B: IntoIterator<Item = I>,
+{
+    let mut a_iter = a.into_iter().peekable();
+    let mut b_iter = b.into_iter().peekable();
+
+    iter::from_fn(move || {
+        // Unfortunately, `Option`'s default Ord impl isn't suitable for this.
+        match (a_iter.peek(), b_iter.peek()) {
+            (Some(a), Some(b)) => {
+                if a < b {
+                    a_iter.next()
+                } else {
+                    b_iter.next()
+                }
+            }
+            (Some(_), None) => a_iter.next(),
+            (None, Some(_)) => b_iter.next(),
+            (None, None) => None,
+        }
+    })
+}
+
+fn leak<T>(value: T) -> &'static T {
+    Box::leak(Box::new(value))
+}
+
+// === ComponentList === //
+
+#[derive(Copy, Clone)]
+struct ComponentType {
+    id: TypeId,
+    dtor: fn(Entity),
+}
+
+impl ComponentType {
+    fn of<T: 'static>() -> Self {
+        fn dtor<T: 'static>(entity: Entity) {
+            drop(entity.remove::<T>()); // (ignores missing components)
+        }
+
+        Self {
+            id: TypeId::of::<T>(),
+            dtor: dtor::<T>,
+        }
+    }
+}
+
+impl Ord for ComponentType {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.id.cmp(&other.id)
+    }
+}
+
+impl PartialOrd for ComponentType {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl hash::Hash for ComponentType {
+    fn hash<H: hash::Hasher>(&self, state: &mut H) {
+        self.id.hash(state);
+    }
+}
+
+impl Eq for ComponentType {}
+
+impl PartialEq for ComponentType {
+    fn eq(&self, other: &Self) -> bool {
+        self.id == other.id
+    }
+}
+
+struct ComponentList {
+    comps: Box<[ComponentType]>,
+    extensions: RefCell<FxHashMap<TypeId, &'static Self>>,
+}
+
+impl hash::Hash for ComponentList {
+    fn hash<H: hash::Hasher>(&self, state: &mut H) {
+        hash_iter(state, self.comps.iter());
+    }
+}
+
+impl Eq for ComponentList {}
+
+impl PartialEq for ComponentList {
+    fn eq(&self, other: &Self) -> bool {
+        self.comps == other.comps
+    }
+}
+
+impl ComponentList {
+    pub fn empty() -> &'static Self {
+        thread_local! {
+            static EMPTY: &'static ComponentList = leak(ComponentList {
+                comps: Box::new([]),
+                extensions: Default::default(),
+            });
+        }
+
+        EMPTY.with(|v| *v)
+    }
+
+    pub fn run_dtors(&self, target: Entity) {
+        for comp in &*self.comps {
+            (comp.dtor)(target);
+        }
+    }
+
+    pub fn de_extend(&'static self, _without: ComponentType) -> &'static Self {
+        // TODO: implement
+        self
+    }
+
+    pub fn extend(&'static self, with: ComponentType) -> &'static Self {
+        if self.comps.contains(&with) {
+            return self;
+        }
+
+        self.extensions
+            .borrow_mut()
+            .entry(with.id)
+            .or_insert_with(|| Self::find_extension_in_db(&self.comps, with))
+    }
+
+    fn find_extension_in_db(base_set: &[ComponentType], with: ComponentType) -> &'static Self {
+        thread_local! {
+            static COMP_LISTS: RefCell<FxHashSet<&'static ComponentList>> = RefCell::new(FxHashSet::from_iter([
+                ComponentList::empty(),
+            ]));
+        }
+
+        struct ComponentListSearch<'a>(&'a [ComponentType], ComponentType);
+
+        impl hash::Hash for ComponentListSearch<'_> {
+            fn hash<H: hash::Hasher>(&self, state: &mut H) {
+                hash_iter(state, merge_iters(self.0, &[self.1]));
+            }
+        }
+
+        impl hashbrown::Equivalent<&'static ComponentList> for ComponentListSearch<'_> {
+            fn equivalent(&self, key: &&'static ComponentList) -> bool {
+                let mut i = 0;
+
+                for entry in &*key.comps {
+                    if entry.id == self.1.id {
+                        continue;
+                    }
+
+                    if entry.id == self.0[i].id {
+                        i += 1;
+                        continue;
+                    }
+
+                    return false;
+                }
+
+                true
+            }
+        }
+
+        COMP_LISTS.with(|set| {
+            *set.borrow_mut()
+                .get_or_insert_with(&ComponentListSearch(base_set, with), |_| {
+                    leak(Self {
+                        comps: merge_iters(base_set.iter().copied(), [with])
+                            .collect::<Vec<_>>()
+                            .into_boxed_slice(),
+                        extensions: Default::default(),
+                    })
+                })
+        })
+    }
+}
+
+// === Storage === //
 
 pub fn storage<T: 'static>() -> &'static Storage<T> {
     thread_local! {
-        static STORAGES: RefCell<FnvHashMap<TypeId, &'static dyn Any>> = Default::default();
+        static STORAGES: RefCell<FxHashMap<TypeId, &'static dyn Any>> = Default::default();
     }
 
     STORAGES.with(|db| {
         db.borrow_mut()
             .entry(TypeId::of::<T>())
-            .or_insert_with(|| &*Box::leak(Box::new(Storage::<T>::default())))
+            .or_insert_with(|| leak(Storage::<T>::default()))
             .downcast_ref::<Storage<T>>()
             .unwrap()
     })
 }
-
-// === Storage === //
 
 const BLOCK_SIZE: usize = 128;
 
 type StorageSlot<T> = RefCell<Option<T>>;
 
 #[derive(Debug)]
-#[derive_where(Default)]
 pub struct Storage<T: 'static>(RefCell<StorageInner<T>>);
 
+impl<T: 'static> Default for Storage<T> {
+    fn default() -> Self {
+        Self(Default::default())
+    }
+}
+
 #[derive(Debug)]
-#[derive_where(Default)]
 struct StorageInner<T: 'static> {
     free_slots: Vec<&'static StorageSlot<T>>,
-    mappings: FnvHashMap<Entity, &'static StorageSlot<T>>,
+    mappings: FxHashMap<Entity, &'static StorageSlot<T>>,
+}
+
+impl<T: 'static> Default for StorageInner<T> {
+    fn default() -> Self {
+        Self {
+            free_slots: Vec::default(),
+            mappings: FxHashMap::default(),
+        }
+    }
 }
 
 impl<T: 'static> Storage<T> {
@@ -48,10 +253,10 @@ impl<T: 'static> Storage<T> {
         let me = &mut *self.0.borrow_mut();
 
         let slot = match me.mappings.entry(entity) {
-            hash_map::Entry::Occupied(entry) => entry.get(),
-            hash_map::Entry::Vacant(entry) => {
+            hashbrown::hash_map::Entry::Occupied(entry) => entry.get(),
+            hashbrown::hash_map::Entry::Vacant(entry) => {
                 if me.free_slots.is_empty() {
-                    let block = repeat_with(StorageSlot::default)
+                    let block = iter::repeat_with(StorageSlot::default)
                         .take(BLOCK_SIZE)
                         .collect::<Vec<_>>()
                         .leak();
@@ -118,14 +323,26 @@ impl<T: 'static> Storage<T> {
 
 // === Entity === //
 
+thread_local! {
+    static ALIVE: RefCell<FxHashMap<Entity, &'static ComponentList>> = Default::default();
+}
+
 #[derive(Debug, Copy, Clone, Hash, Eq, PartialEq)]
 pub struct Entity(NonZeroU64);
 
 impl Entity {
+    pub fn new() -> OwnedEntity {
+        OwnedEntity::new()
+    }
+
     pub fn new_unguarded() -> Self {
         static ID_GEN: AtomicU64 = AtomicU64::new(1);
 
-        Self(NonZeroU64::new(ID_GEN.fetch_add(1, Ordering::Relaxed)).unwrap())
+        let me = Self(NonZeroU64::new(ID_GEN.fetch_add(1, Ordering::Relaxed)).unwrap());
+
+        ALIVE.with(|slots| slots.borrow_mut().insert(me, ComponentList::empty()));
+
+        me
     }
 
     pub fn with<T: 'static>(self, comp: T) -> Self {
@@ -134,10 +351,28 @@ impl Entity {
     }
 
     pub fn insert<T: 'static>(self, comp: T) -> Option<T> {
+        ALIVE.with(|slots| {
+            let mut slots = slots.borrow_mut();
+            let slot = slots.get_mut(&self).unwrap_or_else(|| {
+                panic!("attempted to attach a component to the dead {:?}.", self)
+            });
+
+            *slot = slot.extend(ComponentType::of::<T>());
+        });
+
         storage::<T>().insert(self, comp)
     }
 
     pub fn remove<T: 'static>(self) -> Option<T> {
+        ALIVE.with(|slots| {
+            let mut slots = slots.borrow_mut();
+            let slot = slots.get_mut(&self).unwrap_or_else(|| {
+                panic!("attempted to remove a component from the dead {:?}.", self)
+            });
+
+            *slot = slot.de_extend(ComponentType::of::<T>());
+        });
+
         storage::<T>().remove(self)
     }
 
@@ -162,11 +397,18 @@ impl Entity {
     }
 
     pub fn is_alive(self) -> bool {
-        true // TODO
+        ALIVE.with(|slots| slots.borrow().contains_key(&self))
     }
 
     pub fn destroy(self) {
-        // TODO
+        ALIVE.with(|slots| {
+            let comp_list = slots
+                .borrow_mut()
+                .remove(&self)
+                .unwrap_or_else(|| panic!("attempted to destroy the already-dead {:?}.", self));
+
+            comp_list.run_dtors(self);
+        });
     }
 }
 
